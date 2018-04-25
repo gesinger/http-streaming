1c79cf89c437d901cd856474c7ff4ae990d19b6f
ldayananda <ldayananda@brightcove.com>
2018-04-25 17:29:31 -0400

    add tfdt, remove moof from top-level

--- src/box-parsers.js
+++ src/box-parsers.js
@@ -224,6 +224,19 @@ const tfhd = (data) => {
   return result;
 };
 
+const tfdt = (data) => {
+  var result = {
+    version: data[0],
+    flags: new Uint8Array(data.subarray(1, 4)),
+    baseMediaDecodeTime: data[4] << 24 | data[5] << 16 | data[6] << 8 | data[7]
+  };
+  if (result.version === 1) {
+    result.baseMediaDecodeTime *= Math.pow(2, 32);
+    result.baseMediaDecodeTime += data[8] << 24 | data[9] << 16 | data[10] << 8 | data[11];
+  }
+  return result;
+};
+
 const sdtp = (data) => {
   const result = {
     version: data[0],
@@ -309,11 +322,6 @@ const inspectMp4 = ({ data, isEndOfSegment, topLevelBoxes }) => {
       topLevelBoxes.sidx = box;
       continue;
     }
-    // TODO: might need to remove this from topLevelBoxes
-    if (box.type === 'moof') {
-      topLevelBoxes.moof = box;
-      continue;
-    }
 
     newBoxes.push(box);
   }
@@ -331,9 +339,9 @@ const inspectMp4 = ({ data, isEndOfSegment, topLevelBoxes }) => {
   }
 
   // Now that we have an mdat, we can append
-  (result.boxes.unused || []).concat(newBoxes);
+  const innerBoxes = (result.boxes.unused || []).concat(newBoxes);
 
-  result.bytes = makeMp4Fragment(topLevelBoxes, newBoxes);
+  result.bytes = makeMp4Fragment(topLevelBoxes, innerBoxes);
 
   return result;
 };
@@ -367,6 +375,7 @@ const boxParsers = {
   traf,
   trun,
   tfhd,
+  tfdt,
   sdtp,
   nalParse,
   inspectMp4
